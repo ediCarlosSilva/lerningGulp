e2aa427dfd093331d573683cd68dba0f60d4ff89
Edi Carlos note <edi_csilva@hotmail.com>
2018-07-19 15:54:10 -0300

    automatizado tarefa de apagar pasta dist, copiar past src para dist, minificar imagens

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,20 @@ var gulp = require('gulp'),
     clean = require('gulp-clean');
 
 gulp.task('copy', ['clean'], function() {
-    gulp.src('src/**/*')
+
+    return gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function() {
-    var stream = gulp.src('dist')
-        .pipe(clean());
-    return stream;
-});
 
-gulp.task('build-img', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+})
+
+gulp.task('build-img', ['copy'], function() {
+
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('dist/img'));
 });
