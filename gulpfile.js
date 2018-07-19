0d71bb8bf12c6827cc129bc62922988f2cd9627c
Edi Carlos note <edi_csilva@hotmail.com>
2018-07-19 10:21:28 -0300

    ajustes iniciais no gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('build-img', function() {
-
     gulp.src('src/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
-
+        .pipe(gulp.dest('src/img'))
 });
